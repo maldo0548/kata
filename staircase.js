20ce02405c03b0c400fd00f27fae143ee9a24204
Miguel Maldonado <maldonado.miguel45@gmail.com>
2018-06-14 14:50:44 -0400

    Solved Staircase challenge

--- staircase.js
+++ staircase.js
@@ -1,5 +1,16 @@
-// Write a program that prints a staircase of size n.
+// Write a program that prints a staircase of size n aligned to the right.
 const n = 6;
-function staircase(n) {}
+function staircase(n) {
+  let i = 1;
+  while (i <= n) {
+    const total = n;
+    let symbols = i;
+    let spaces = total - symbols;
+    let blank = " ".repeat(spaces);
+    let line = `#`.repeat(symbols);
+    console.log(blank + line);
+    i++;
+  }
+}
 
 staircase(n);
