1689d4bda2b1c67355afabf0e8ab6c40f46e8d25
Miguel Maldonado <maldonado.miguel45@gmail.com>
2018-06-14 17:05:09 -0400

    Found the real solution to the problem

--- mini-max-sum.js
+++ mini-max-sum.js
@@ -7,16 +7,23 @@ If we sum everything except 4, our sum is 1 + 2 + 3 + 5 = 11 .
 If we sum everything except 5, our sum is 1 + 2 + 3 + 4 = 10 .
 Output the range of the min and maximum values in an array with two integers.
 */
-const control = [34, 12, 67, 13, 23];
+const control = [1, 2, 3, 4, 5];
+const problem = [2, 2, 2, 2, 2];
+const problem2 = [5, 5, 5, 5, 5];
+
 function miniMaxSum(arr) {
   let storage = [];
+  let filteredSum = [arr[0] * (arr.length - 1)];
+
   for (var i = 0; i < arr.length; i++) {
     let filteredArray = arr.filter(num => num != arr[i]);
-
-    let filteredSum = filteredArray.reduce(function(prev, cur) {
-      return prev + cur;
-    });
-
+    if (filteredArray.length > 1) {
+      filteredSum = filteredArray.reduce(function(prev, cur) {
+        return prev + cur;
+      });
+    }
+    // console.log(filteredArray);
+    // console.log(`storage.push(filteredSum): ${storage.push(filteredSum)}`);
     storage.push(filteredSum);
   }
   let max = storage.reduce(function(a, b) {
@@ -30,3 +37,5 @@ function miniMaxSum(arr) {
   return result;
 }
 miniMaxSum(control);
+miniMaxSum(problem);
+miniMaxSum(problem2);
