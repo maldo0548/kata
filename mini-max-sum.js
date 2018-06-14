e0724e493e8e49506446e779e7a30d5e820c32a4
Miguel Maldonado <maldonado.miguel45@gmail.com>
2018-06-14 16:13:56 -0400

    solved mini-max-sum

--- mini-max-sum.js
+++ mini-max-sum.js
@@ -7,5 +7,26 @@ If we sum everything except 4, our sum is 1 + 2 + 3 + 5 = 11 .
 If we sum everything except 5, our sum is 1 + 2 + 3 + 4 = 10 .
 Output the range of the min and maximum values in an array with two integers.
 */
+const control = [34, 12, 67, 13, 23];
+function miniMaxSum(arr) {
+  let storage = [];
+  for (var i = 0; i < arr.length; i++) {
+    let filteredArray = arr.filter(num => num != arr[i]);
 
-function miniMaxSum(arr) {}
+    let filteredSum = filteredArray.reduce(function(prev, cur) {
+      return prev + cur;
+    });
+
+    storage.push(filteredSum);
+  }
+  let max = storage.reduce(function(a, b) {
+    return Math.max(a, b);
+  });
+  let min = storage.reduce(function(a, b) {
+    return Math.min(a, b);
+  });
+  let result = [min, max];
+  console.log(result);
+  return result;
+}
+miniMaxSum(control);
