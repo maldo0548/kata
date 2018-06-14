9b118681723450bc06902a16e38200cba12640bc
Miguel Maldonado <maldonado.miguel45@gmail.com>
2018-06-14 15:01:12 -0400

    adding mini-max-sum file

---
+++ mini-max-sum.js
@@ -0,0 +1,11 @@
+/*
+Our initial numbers are 1, 2, 3, 4, and 5. We can calculate the following sums using four of the five integers:
+If we sum everything except 1, our sum is 2 + 3 + 4 + 5 = 14 .
+If we sum everything except 2, our sum is 1 + 3 + 4 + 5 = 13 .
+If we sum everything except 3, our sum is 1 + 2 + 4 + 5 = 12 .
+If we sum everything except 4, our sum is 1 + 2 + 3 + 5 = 11 .
+If we sum everything except 5, our sum is 1 + 2 + 3 + 4 = 10 .
+Output the range of the min and maximum values in an array with two integers.
+*/
+
+function miniMaxSum(arr) {}
